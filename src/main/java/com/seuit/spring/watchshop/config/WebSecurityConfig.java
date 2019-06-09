628235eedd48126e557a03361d9c512457d6fc9a
Duy Anh <danhy989@gmail.com>
2019-06-09 19:26:14 +0700

    up final

--- src/main/java/com/seuit/spring/watchshop/config/WebSecurityConfig.java
+++ src/main/java/com/seuit/spring/watchshop/config/WebSecurityConfig.java
@@ -11,13 +11,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import com.seuit.spring.watchshop.service.UserDetailServiceImpl;
-
-import io.micrometer.core.ipc.http.HttpSender.Method;
 
 @Configuration
 @EnableWebSecurity
